d1d81b740c05b633a5f30f08cc96d46daabd370c
THWU <grinti0730@gmail.com>
2017-06-08 11:10:11 +0800

    update comment

--- index.js
+++ index.js
@@ -12,51 +12,42 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/authors', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     authors.index(req, res);
 });
 
 app.get('/authors/:id', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     authors.show(req, res);
 });
 
 app.post('/authors', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     authors.create(req, res);
 });
 
 app.put('/authors', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     authors.update(req, res);
 });
 app.delete('/authors', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     authors.delete(req, res);
 });
 
 app.get('/books', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     books.index(req, res);
 });
 app.get('/books/:id', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     books.show(req, res);
 });
 app.post('/books', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     books.create(req, res);
 });
 app.delete('/books', urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    // create user in req.body
     books.delete(req, res);
 });
 
